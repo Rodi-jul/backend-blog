6501e6a7521b687fec5cef40d0cda129c3588ecf
Rodi-jul <julia.rodionova.1997@yandex.ru>
2020-04-22 11:59:21 +0300

    + default.env, + dotenv
    dotenv needed for working with configuration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+const root = path.join.bind(this, __dirname, '../');
+dotenv.config({ path: root('.env') });
 
 const app = express();
 
 app.get('/', (request, response) => {
-	response.send('Hello world!');
+	response.send('Hello, Julie!');
 });
 
-app.listen(7777, () => {
-	console.log('Express server listening on port 7777');
+app.listen(process.env.PORT, () => {
+	console.log(`Express server run HTTP://${process.env.HOST}:${process.env.PORT}`);
 });
